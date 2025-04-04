20bae5ea3d37264a8d654878db609d018c0593e4
adam vietro <hunterafi@gmail.com>
2025-04-03 18:53:05 -0700

    Finished adding in all the __MODULE__ to every test case and made it still work for the general Agent.

--- test/advanced_score_tracker_test.exs
+++ test/advanced_score_tracker_test.exs
@@ -4,60 +4,60 @@ defmodule AdvancedScoreTrackerTest do
   doctest AdvancedScoreTracker
 
   setup do
-    AdvancedScoreTracker.start_link(__MODULE__)
+    AdvancedScoreTracker.start_link(name: __MODULE__)
     :ok
   end
 
   describe "Test Cases" do
     test "add/3" do
-      assert :ok == AdvancedScoreTracker.add(:player1, :ping_pong, 10)
+      assert :ok == AdvancedScoreTracker.add(:player1, :ping_pong, 10, __MODULE__)
     end
 
     test "get/2" do
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      assert 10 == AdvancedScoreTracker.get(:player2, :ping_pong)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      assert 10 == AdvancedScoreTracker.get(:player2, :ping_pong, __MODULE__)
     end
 
     test "get/2 with 2 players" do
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      assert 20 == AdvancedScoreTracker.get(:player2, :ping_pong)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      assert 20 == AdvancedScoreTracker.get(:player2, :ping_pong, __MODULE__)
 
-      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10)
-      assert 10 == AdvancedScoreTracker.get(:player1, :ping_pong)
+      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10, __MODULE__)
+      assert 10 == AdvancedScoreTracker.get(:player1, :ping_pong, __MODULE__)
     end
 
     test "new_game/2" do
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10, __MODULE__)
 
-      :ok = AdvancedScoreTracker.new(:player1, :ping_pong)
-      :ok = AdvancedScoreTracker.new(:player2, :ping_pong)
+      :ok = AdvancedScoreTracker.new(:player1, :ping_pong, __MODULE__)
+      :ok = AdvancedScoreTracker.new(:player2, :ping_pong, __MODULE__)
 
-      assert 0 == AdvancedScoreTracker.get(:player2, :ping_pong)
-      assert 0 == AdvancedScoreTracker.get(:player1, :ping_pong)
+      assert 0 == AdvancedScoreTracker.get(:player2, :ping_pong, __MODULE__)
+      assert 0 == AdvancedScoreTracker.get(:player1, :ping_pong, __MODULE__)
     end
 
     test "history/2" do
-      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.new(:player1, :ping_pong)
+      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.new(:player1, :ping_pong, __MODULE__)
 
-      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10)
+      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.add(:player1, :ping_pong, 10, __MODULE__)
 
-      :ok = AdvancedScoreTracker.new(:player1, :ping_pong)
-      assert [20, 10] == AdvancedScoreTracker.history(:player1, :ping_pong)
+      :ok = AdvancedScoreTracker.new(:player1, :ping_pong, __MODULE__)
+      assert [20, 10] == AdvancedScoreTracker.history(:player1, :ping_pong, __MODULE__)
     end
 
     test "high_score/2" do
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.new(:player2, :ping_pong)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.new(:player2, :ping_pong, __MODULE__)
 
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10)
-      :ok = AdvancedScoreTracker.new(:player2, :ping_pong)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.add(:player2, :ping_pong, 10, __MODULE__)
+      :ok = AdvancedScoreTracker.new(:player2, :ping_pong, __MODULE__)
 
-      assert 20 == AdvancedScoreTracker.high_score(:player2, :ping_pong)
+      assert 20 == AdvancedScoreTracker.high_score(:player2, :ping_pong, __MODULE__)
     end
   end
 end
