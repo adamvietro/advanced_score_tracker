880fa76f555b69f66847e6f7ca9956d9b633c82f
adam vietro <hunterafi@gmail.com>
2025-04-03 18:46:56 -0700

    Current state of the project.

--- test/advanced_score_tracker_test.exs
+++ test/advanced_score_tracker_test.exs
@@ -4,11 +4,11 @@ defmodule AdvancedScoreTrackerTest do
   doctest AdvancedScoreTracker
 
   setup do
-    AdvancedScoreTracker.start_link()
+    AdvancedScoreTracker.start_link(__MODULE__)
     :ok
   end
 
-  # describe "Test Cases" do
+  describe "Test Cases" do
     test "add/3" do
       assert :ok == AdvancedScoreTracker.add(:player1, :ping_pong, 10)
     end
@@ -59,5 +59,5 @@ defmodule AdvancedScoreTrackerTest do
 
       assert 20 == AdvancedScoreTracker.high_score(:player2, :ping_pong)
     end
-  # end
+  end
 end
